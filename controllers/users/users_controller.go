fb4ce41d83e0f64860b996485676789427b304f9
nakadayoshiki <y.nakada.rkt@gmail.com>
2020-05-15 22:25:14 +0900

    fixed

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nakadayoshiki/bookstore_users-api/domain/users"
+	"github.com/nakadayoshiki/bookstore_users-api/services"
+	"github.com/nakadayoshiki/bookstore_users-api/utils/errors"
 )
 
 func CreateUser(c *gin.Context) {
@@ -11,5 +14,16 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
